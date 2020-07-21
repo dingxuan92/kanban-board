be313bfc328675e0e12cd911200bfc2d6ca6d1ff
chandingxuan928 <chandingxuan928@ocft.com>
2020-07-21 20:07:18 +0800

    set initial state

--- src/App.js
+++ src/App.js
@@ -9,7 +9,61 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      //initial states
+      stagesTasks: {
+        "stage-0": [
+          {
+            "data-testid": "task-task0",
+            "name": "task 0"
+          },
+          {
+            "data-testid": "task-task0",
+            "name": "task 0"
+          },
+          {
+            "data-testid": "task-task1",
+            "name": "task 1"
+          },
+          {
+            "data-testid": "task-task2",
+            "name": "task 2"
+          },
+          {
+            "data-testid": "task-task3",
+            "name": "task 3"
+          }
+        ],
+        "stage-1": [
+          {
+            "data-testid": "task-task4",
+            "name": "task 4"
+          },
+          {
+            "data-testid": "task-task5",
+            "name": "task 5"
+          },
+          {
+            "data-testid": "task-task6",
+            "name": "task 6"
+          }
+        ],
+        "stage-2": [
+          {
+            "data-testid": "task-task7",
+            "name": "task 7"
+          },
+          {
+            "data-testid": "task-task8",
+            "name": "task 8"
+          }
+        ],
+        "stage-3": [
+          {
+            "data-testid": "task-task9",
+            "name": "task 9"
+          }
+        ]
+      }
     };
     this.stagesNames = ['Backlog', 'To Do', 'Ongoing', 'Done'];
   }
@@ -20,8 +74,7 @@ class App extends Component {
     return (
       <div className="App">
         <Controls />
-        <Board stagesNames={this.stagesNames}/>
-
+        <Board stagesNames={this.stagesNames} stagesTasks={this.state.stagesTasks}/>
       </div>
     );
   }
